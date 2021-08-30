a5dd1ded0f66cdf9084ad6dfc6f10d04e2a55469
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-30 21:31:31 +0700

    Add iframe widgets

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,18 +1,37 @@
-import * as path from 'path'
-import { app, BrowserWindow } from 'electron'
+import * as path from "path"
+import electron, { app, BrowserWindow } from "electron"
 
 const loadURL = import.meta.env.PROD
-  ? `file://${path.resolve(__dirname, '../renderer/index.html')}`
-  : process['env'].RENDERER_URL || 'http://localhost:3000'
+  ? `file://${path.resolve(__dirname, "../renderer/index.html")}`
+  : process["env"].RENDERER_URL || "http://localhost:3000"
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({ 
-    width: 800,
-    height: 600,
-    icon: path.resolve(__dirname, '../../public/icons/256x256.png')
+  const screenSize = electron.screen.getPrimaryDisplay().size
+  const mainWindow = new BrowserWindow({
+    width: screenSize.width,
+    height: screenSize.height,
+    icon: path.resolve(__dirname, "../../public/icons/256x256.png"),
+    transparent: true,
+    frame: false,
+    // alwaysOnTop: true,
+    hasShadow: false,
   })
 
   mainWindow.loadURL(loadURL)
+  // mainWindow.setIgnoreMouseEvents(true) // Click Through
+
+  mainWindow.webContents.session.webRequest.onHeadersReceived(
+    { urls: ["*://*/*"] },
+    (d, c) => {
+      if (d.responseHeaders?.["X-Frame-Options"]) {
+        delete d.responseHeaders["X-Frame-Options"]
+      } else if (d.responseHeaders?.["x-frame-options"]) {
+        delete d.responseHeaders["x-frame-options"]
+      }
+
+      c({ cancel: false, responseHeaders: d.responseHeaders })
+    }
+  )
 
   // mainWindow.webContents.openDevTools()
 }
@@ -20,11 +39,11 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow()
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit()
 })
--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -3,21 +3,10 @@
   import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+<main style="width: 100%; height: 100vh;">
+  <iframe style="display: fixed;" src="https://streamkit.discord.com/overlay/chat/826099393694400574/848810430683938876?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=25&invite_code=9arm&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0" width="400px" height="400px" frameborder="0"></iframe>
+  <iframe style="display: fixed;" src="https://ko-fi.com/streamalerts/overlay/sa_6cd116dc-8c8b-4b44-a7cd-5e2371d32447" width="400px" height="400px" frameborder="0"></iframe>
+  <iframe style="display: fixed;" src="https://streamkit.discord.com/overlay/voice/826099393694400574/826106139859091536?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=25&invite_code=9arm&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0" width="400px" height="400px" frameborder="0"></iframe>
 </main>
 
 <style>
@@ -26,40 +15,4 @@
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
