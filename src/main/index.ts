e14381cb1a17cfef64571c57081d533d33018844
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-30 21:48:41 +0700

    Add global shortcuts for debugging

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,5 @@
 import * as path from "path"
-import electron, { app, BrowserWindow } from "electron"
+import electron, { app, BrowserWindow, globalShortcut } from "electron"
 
 const loadURL = import.meta.env.PROD
   ? `file://${path.resolve(__dirname, "../renderer/index.html")}`
@@ -13,12 +13,12 @@ function createWindow() {
     icon: path.resolve(__dirname, "../../public/icons/256x256.png"),
     transparent: true,
     frame: false,
-    // alwaysOnTop: true,
+    alwaysOnTop: true,
     hasShadow: false,
   })
 
   mainWindow.loadURL(loadURL)
-  // mainWindow.setIgnoreMouseEvents(true) // Click Through
+  mainWindow.setIgnoreMouseEvents(true) // Click Through
 
   mainWindow.webContents.session.webRequest.onHeadersReceived(
     { urls: ["*://*/*"] },
@@ -33,14 +33,42 @@ function createWindow() {
     }
   )
 
-  // mainWindow.webContents.openDevTools()
+  return mainWindow
 }
 
+let window: electron.BrowserWindow
+let transparentBackground = true
+let debugMode = false
+
 app.whenReady().then(() => {
-  createWindow()
+  window = createWindow()
 
   app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+      window = createWindow()
+    }
+  })
+
+  globalShortcut.register("Alt+CommandOrControl+F", () => {
+    transparentBackground = !transparentBackground
+    window.setBackgroundColor(transparentBackground ? "#00FFFFFF" : "#FFFFFFFF")
+  })
+
+  globalShortcut.register("Alt+CommandOrControl+V", () => {
+    debugMode = !debugMode
+
+    window.setAlwaysOnTop(!debugMode)
+    window.setIgnoreMouseEvents(!debugMode)
+  })
+
+  globalShortcut.register("Alt+CommandOrControl+L", () => {
+    const devTools = window.webContents.isDevToolsOpened()
+
+    if (devTools) {
+      window.webContents.closeDevTools()
+    } else {
+      window.webContents.openDevTools()
+    }
   })
 })
 
