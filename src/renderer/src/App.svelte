adfed60a74fe636a3847678207c5dd77bdca2a33
Manassarn Manoonchai <manassarn@gmail.com>
2021-09-14 21:29:04 +0700

    Save widget location and size

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -20,7 +20,9 @@
             })
 
             Object.assign(event.target.dataset, { x, y })
-          }
+
+            updateWidgetsData({ id: event.target.parentNode.id, w: event.rect.width, h: event.rect.height, x, y })
+          },
         }
 
       })
@@ -39,26 +41,39 @@
           event.target.parentNode.style.transform =
             `translate(${x}px, ${y}px)`
 
-            Object.assign(event.target.dataset, { x, y })
+          Object.assign(event.target.dataset, { x, y })
 
+          updateWidgetsData({ id: event.target.parentNode.id, w: event.rect.width, h: event.rect.height, x, y })
         },
       }
-})
-
-
+    })
   })
+
+  function updateWidgetsData({id, x, y, w, h}) {
+    const localStorageKey = "widgets"
+    const widgetsData = JSON.parse(localStorage.getItem(localStorageKey) || "{}")
+
+    widgetsData[id] = {
+      x,
+      y,
+      w,
+      h,
+    }
+
+    localStorage.setItem(localStorageKey, JSON.stringify(widgetsData))
+  }
 </script>
 
 <main style="width: 100%; height: 100vh;">
-  <div style="width: 600px; height: 600px; position: fixed; overflow: hidden;">
+  <div id="widget-1" style="width: 600px; height: 600px; position: fixed; overflow: hidden;">
     <div class="draggable resizable" style="width: 100%; height: 100%; background-color: rgba(100, 0, 0, 0); z-index: 100; position: absolute;"></div>
     <iframe title="frame_1" style="position: fixed; top: 0; z-index: 0" src="https://streamkit.discord.com/overlay/chat/826099393694400574/848810430683938876?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=25&invite_code=9arm&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0" width="580px" height="215px" frameborder="0"></iframe>
   </div>
-  <div style="width: 600px; height: 600px; position: fixed; overflow: hidden;">
+  <div id="widget-2" style="width: 600px; height: 600px; position: fixed; overflow: hidden;">
     <div class="draggable resizable" style="width: 100%; height: 100%; background-color: rgba(100, 0, 0, 0); z-index: 100; position: fixed;"></div>
     <iframe title="frame_2" style="display: fixed;" src="https://ko-fi.com/streamalerts/overlay/sa_6cd116dc-8c8b-4b44-a7cd-5e2371d32447" width="400px" height="400px" frameborder="0"></iframe>
   </div>
-  <div style="width: 600px; height: 600px; position: fixed; overflow: hidden;">
+  <div id="widget-3" style="width: 600px; height: 600px; position: fixed; overflow: hidden;">
     <div class="draggable resizable" style="width: 100%; height: 100%; background-color: rgba(100, 0, 0, 0); z-index: 100; position: absolute;"></div>
     <iframe title="frame_3" style="display: fixed;" src="https://streamkit.discord.com/overlay/voice/826099393694400574/826106139859091536?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=25&invite_code=9arm&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0" width="400px" height="400px" frameborder="0"></iframe>
   </div>
